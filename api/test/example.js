9991ed2cc31a7b8d306a0160a2c40cca1fd08306
Jay R Bolton <jayrbolton@gmail.com>
2018-09-26 10:36:50 -0700

    Add more tests

--- api/test/example.js
+++ api/test/example.js
@@ -24,81 +24,118 @@ describe('provenance-based object viewing permissions', function () {
   it('works', function () {
     // Create user1
     const password = 'password'
-    let response = request.post(`${baseUrl}/auth/signup`, {
-      json: true,
-      body: {email: 'user1@example.com', password}
-    })
-    if (response.statusCode !== 409 && response.statusCode !== 200) {
-      // A 409 is okay; user already exists
-      throw new Error('Invalid signup response: ' + String(response.body))
-    }
-    // Create user2
-    response = request.post(`${baseUrl}/auth/signup`, {
-      json: true,
-      body: {email: 'user2@example.com', password}
-    })
+    // let response = request.post(`${baseUrl}/auth/signup`, {
+    //   json: true,
+    //   body: {email: 'user1@example.com', password}
+    // })
+    // if (response.statusCode !== 409 && response.statusCode !== 200) {
+    //   // A 409 is okay; user already exists
+    //   throw new Error('Invalid signup response: ' + String(response.body))
+    // }
+    // // Create user2
+    // response = request.post(`${baseUrl}/auth/signup`, {
+    //   json: true,
+    //   body: {email: 'user2@example.com', password}
+    // })
+    // if (response.statusCode !== 409 && response.statusCode !== 200) {
+    //   // A 409 is okay; user already exists
+    //   throw new Error('Invalid signup response: ' + String(response.body))
+    // }
+    let response
     response = request.post(`${baseUrl}/auth/login`, {
       json: true,
       body: {email: 'user1@example.com', password}
     })
+    if (response.statusCode !== 200) {
+      throw new Error('Invalid login response: ' + String(response.body))
+    }
     let token = response.headers['x-session-id']
     response = request.post(`${baseUrl}/workspaces`, {
       json: true,
       body: {name: 'workspace1'},
       headers: {'x-session-id': token}
     })
+    if (response.statusCode !== 201) {
+      throw new Error(`Invalid workspace creation response (code was ${response.statusCode}): ${String(response.body)}`)
+    }
     const workspace1 = JSON.parse(response.body)._key
     response = request.post(`${baseUrl}/workspaces/${workspace1}/objects`, {
       json: true,
       body: {name: 'object1'},
       headers: {'x-session-id': token}
     })
+    if (response.statusCode !== 201) {
+      throw new Error(`Invalid object creation response (code was ${response.statusCode}): ${String(response.body)}`)
+    }
     const object1 = JSON.parse(response.body)._key
     response = request.post(`${baseUrl}/workspaces/${workspace1}/copy`, {
       json: true,
       headers: {'x-session-id': token}
     })
+    if (response.statusCode !== 200) {
+      throw new Error(`Invalid workspace copy response (code was ${response.statusCode}): ${String(response.body)}`)
+    }
     const workspace2 = JSON.parse(response.body)._key
     response = request.put(`${baseUrl}/workspaces/${workspace2}/objects/${object1}`, {
       json: true,
       body: {name: 'object2'},
       headers: {'x-session-id': token}
     })
+    if (response.statusCode !== 200) {
+      throw new Error(`Invalid object update response (code was ${response.statusCode}): ${String(response.body)}`)
+    }
     const object2 = JSON.parse(response.body)._key
     response = request.delete(`${baseUrl}/workspaces/${workspace2}/objects/${object1}`, {
       json: true,
       headers: {'x-session-id': token}
     })
+    if (response.statusCode !== 200) {
+      throw new Error(`Invalid object delete response (code was ${response.statusCode}): ${String(response.body)}`)
+    }
     response = request.post(`${baseUrl}/workspaces/${workspace2}/viewer`, {
       json: true,
       body: 'user2@example.com',
       headers: {'x-session-id': token}
     })
+    if (response.statusCode !== 200) {
+      throw new Error(`Invalid viewer creation response (code was ${response.statusCode}): ${String(response.body)}`)
+    }
 
     request.post(`${baseUrl}/auth/logout`)
     response = request.post(`${baseUrl}/auth/login`, {
       json: true,
       body: {email: 'user2@example.com', password: 'password'}
     })
+    if (response.statusCode !== 200) {
+      throw new Error(`Invalid login response (code was ${response.statusCode}): ${String(response.body)}`)
+    }
     token = response.headers['x-session-id']
 
     response = request.get(`${baseUrl}/workspaces`, {
       json: true,
       headers: {'x-session-id': token}
     })
+    if (response.statusCode !== 200) {
+      throw new Error(`Invalid workspace fetch response (code was ${response.statusCode}): ${String(response.body)}`)
+    }
     const ws = JSON.parse(response.body)
       .map(ws => ws._key)
       .filter(key => key === workspace1 || key === workspace2)
     assert.equal(ws.length, 1)
 
+    console.log('object1, object2', object1, object2)
     response = request.get(`${baseUrl}/objects`, {
       json: true,
       headers: {'x-session-id': token}
     })
+    if (response.statusCode !== 200) {
+      throw new Error(`Invalid object fetch response (code was ${response.statusCode}): ${String(response.body)}`)
+    }
     const objs = JSON.parse(response.body)
       .map(o => o._key)
       .filter(key => key === object1 || key === object2)
 
+    console.log('final objects:', objs)
     assert.equal(objs.length, 2)
   })
 })
